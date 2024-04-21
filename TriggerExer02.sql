9f0ca06f1a2a3e29a227e6dc52985dd0b8da8f69
Luan Camilo Nogueira <99506287+Felliny@users.noreply.github.com>
2024-04-21 15:56:53 -0300

    Update TriggerExer02.sql

--- TriggerExer02.sql
+++ TriggerExer02.sql
@@ -6,23 +6,23 @@ create table produto(
     codigo          int             not null,
     nome            varchar(40)     not null,
     descricao       varchar(100)    not null ,
-    valor_unitario  decimal(7, 2)   not null ,
+    valor_unitario  decimal(7, 2)   not null 
     primary key (codigo)
 )
 go
 create table estoque(
     codigo_produto          int         not null ,
     qtd_estoque             int         not null ,
-    estoque_min             int         not null ,
-    primary key (codigo_produto),
+    estoque_min             int         not null 
+    primary key (codigo_produto)
     foreign key (codigo_produto) references produto(codigo)
 )
 go
 create table venda(
     nota_fiscal         int         not null ,
     codigo_produto      int         not null ,
-    quantidade          int         not null ,
-    primary key (nota_fiscal),
+    quantidade          int         not null 
+    primary key (nota_fiscal)
     foreign key (codigo_produto) references produto(codigo)
 )
 
